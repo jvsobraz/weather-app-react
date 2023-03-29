5298335622ba550480825cd249e8489339b63aa8
jvsobraz <jvsobraz@hotmail.com>
2023-03-29 09:34:31 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  //const url ='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e5d55877d7ef290e791dd048dfd1bbaa'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>São Paulo</p>
+          </div>
+          <div className="temp">
+            <h1>25°C</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>30°C</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
